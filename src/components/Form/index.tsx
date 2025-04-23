2bc406cb75e200c6520a050ce05e7d6d1634c860
J3eff <jeffersonvideo125@gmail.com>
2025-04-23 18:10:58 -0300

    Criando a lista

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "../Button";
 
-class Form extends React.Component {
+export default class Form extends React.Component {
   render() {
     return (
       <form>
@@ -34,5 +34,3 @@ class Form extends React.Component {
     );
   }
 }
-
-export default Form;
